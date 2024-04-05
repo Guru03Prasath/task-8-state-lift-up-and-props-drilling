4950aa0300e416b2dd747180563246050cc08ac1
GURU PRASATH N <160146277+Guru03Prasath@users.noreply.github.com>
2024-04-05 11:50:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { useState } from 'react';
+import ParentComponent from './ParentComponent';
+
+function App() {
+  return (
+    <div>
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ ChildComponent.js
@@ -0,0 +1,13 @@
+// ChildComponent.js
+import React from 'react';
+
+function ChildComponent({ incrementCount }) {
+  return (
+    <div>
+      <h3>Child Component</h3>
+      <button onClick={incrementCount}>Increment Count</button>
+    </div>
+  );
+}
+
+export default ChildComponent;
